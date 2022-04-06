f5cf59f52413bc723750da8424bb992a1439dbf6
Shibu4064 <hrithikmajumdershibu@gmail.com>
2022-04-06 19:51:27 +0600

    new

--- job_portal.sql
+++ job_portal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 27, 2022 at 02:22 PM
+-- Generation Time: Apr 06, 2022 at 03:48 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.6
 
@@ -78,18 +78,20 @@ CREATE TABLE `all_jobs` (
   `description` varchar(255) NOT NULL,
   `country` varchar(255) NOT NULL,
   `city` varchar(255) NOT NULL,
-  `area` varchar(255) NOT NULL
+  `area` varchar(255) NOT NULL,
+  `keyword` varchar(111) NOT NULL,
+  `category` varchar(111) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `all_jobs`
 --
 
-INSERT INTO `all_jobs` (`job_id`, `customer_email`, `job_title`, `description`, `country`, `city`, `area`) VALUES
-(1, 'khona1180@gmail.com', 'PHP DEVELOPER', 'Website Development & Customization', 'Bangladesh', 'Dhaka', 'Farmgate'),
-(2, 'hrithik4064@gmail.com', 'Javascript Experts', 'OOP Developer and Swift', 'Bangladesh', 'Khulna', 'Abhaynagar'),
-(3, 'hrithik4064@gmail.com', 'Mern Stack Developer', 'We are hiring some mern stack developer for our company.', 'Bangladesh', 'Sylhet', 'Maulavi Bazar'),
-(5, 'hrithik4064@gmail.com', 'Laravel Dev Ops', 'We need some creative Laravel Developers for our company.', 'Spain', 'Catalonia', 'Barcelona');
+INSERT INTO `all_jobs` (`job_id`, `customer_email`, `job_title`, `description`, `country`, `city`, `area`, `keyword`, `category`) VALUES
+(1, 'khona1180@gmail.com', 'PHP DEVELOPER', 'Website Development & Customization', 'Bangladesh', 'Dhaka', 'Farmgate', 'php', 'developer'),
+(2, 'hrithik4064@gmail.com', 'Javascript Experts', 'OOP Developer and Swift', 'Bangladesh', 'Khulna', 'Abhaynagar', 'java', 'Programmer'),
+(3, 'hrithik4064@gmail.com', 'Mern Stack Developer', 'We are hiring some mern stack developer for our company.', 'Bangladesh', 'Sylhet', 'Maulavi Bazar', 'mern', 'stack developer'),
+(5, 'hrithik4064@gmail.com', 'Laravel Dev Ops', 'We need some creative Laravel Developers for our company.', 'Spain', 'Catalonia', 'Barcelona', 'software', 'Developer');
 
 -- --------------------------------------------------------
 
@@ -100,17 +102,63 @@ INSERT INTO `all_jobs` (`job_id`, `customer_email`, `job_title`, `description`, 
 CREATE TABLE `company` (
   `company_id` int(255) NOT NULL,
   `company` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL
+  `description` varchar(255) NOT NULL,
+  `admin` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `company`
 --
 
-INSERT INTO `company` (`company_id`, `company`, `description`) VALUES
-(1, 'RAZIN TECH', 'Web Developing'),
-(2, 'Trakin Tech', 'Software Developer'),
-(3, 'Rangers IT', 'Freelancing Tools Supplier.');
+INSERT INTO `company` (`company_id`, `company`, `description`, `admin`) VALUES
+(1, 'RAZIN TECH', 'Web Development.', 'ehaaland@gmail.com'),
+(2, 'Trakin Tech', 'Software Developer', 'khona1180@gmail.com'),
+(3, 'Rangers IT', 'Freelancing Tools Supplier.', 'ehaaland@gmail.com'),
+(5, 'Innovex IT', 'Promising software development startup ', 'khona1180@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_category`
+--
+
+CREATE TABLE `job_category` (
+  `id` int(50) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `description` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `job_category`
+--
+
+INSERT INTO `job_category` (`id`, `category`, `description`) VALUES
+(2, 'Software Engineer', 'Software Engineering is one of the most popular profession in the Information Technology sector .'),
+(3, 'Developer', 'lrifhefhfiajfhdihwedoiqjddluhiudhdijiudhiahahdiuahfbwieoywoiyowbeuiyoqueyowvevoywiybweibwoiboweiu.'),
+(4, 'Python', 'Python is one of the most popular programming languages which is mostly used for artificial intelligence and robotics.'),
+(5, 'UI Designer', 'fgifgsduyasdgsuhdysgdiugdsudaydaeiudygwiudgsduadysduoyagosauddgudybaiedyishisud');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_seeker`
+--
+
+CREATE TABLE `job_seeker` (
+  `id` int(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `job_seeker`
+--
+
+INSERT INTO `job_seeker` (`id`, `email`, `password`, `fname`, `lname`, `phone`) VALUES
+(1, 'hrithik4064@gmail.com', '4064', 'Hrithik', 'Majumdar', '123456789');
 
 --
 -- Indexes for dumped tables
@@ -141,6 +189,18 @@ ALTER TABLE `company`
   ADD PRIMARY KEY (`company_id`);
 
 --
+-- Indexes for table `job_category`
+--
+ALTER TABLE `job_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `job_seeker`
+--
+ALTER TABLE `job_seeker`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -160,7 +220,19 @@ ALTER TABLE `all_jobs`
 -- AUTO_INCREMENT for table `company`
 --
 ALTER TABLE `company`
-  MODIFY `company_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `company_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `job_category`
+--
+ALTER TABLE `job_category`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `job_seeker`
+--
+ALTER TABLE `job_seeker`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
